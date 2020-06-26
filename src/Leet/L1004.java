eb4660ddf00d3755a16ad4c6ca96bfe9fcd8b67c
Aashish-Anand <aashishanand1997@gmail.com>
2020-06-26 17:48:27 +0530

    new solution

--- src/Leet/L1004.java
+++ src/Leet/L1004.java
@@ -6,20 +6,64 @@ public class L1004 {
     }
 
     private void run() {
-        int arr[] =  {1,1,1,0,0,0,1,1,1,1,0};
-        int k = 2;
+        int arr[] = {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+        int k = 0;
 
         System.out.println(longestOnes(arr, k));
+        System.out.println(longestOnesApproach2(arr, k));
     }
 
-    private int longestOnes(int arr[], int k){
+    // this is a n^2 approach
+    private int longestOnes(int arr[], int k) {
         int i = 0;
         int j = 0;
-        int tempAllowedChange = k;
-        int n = arr.length;
-        while(i<n) {
 
+        int n = arr.length;
+        int max = 0;
+        while (i < n) {
+            j = i;
+            int tempAllowedChange = k;
+            while (j < n && tempAllowedChange >= 0) {
+                if (arr[j] == 0) {
+                    tempAllowedChange--;
+                    if (tempAllowedChange < 0) {
+                        break;
+                    }
+                }
+                j++;
+            }
+            max = Math.max(max, j - i);
+            //System.out.print((j-i)+" ");
+            i++;
         }
-        return 0;
+        return max;
+    }
+
+    private int longestOnesApproach2(int arr[], int k) {
+        int i = 0;
+        int j = 0;
+        int tempAllowedOnes = k;
+        int n = arr.length;
+        int max = 0;
+        while (j < n) {
+            if(arr[j]==1){
+                j++;
+            }
+            else if(arr[j]==0){
+                tempAllowedOnes--;
+                if(tempAllowedOnes<0) {
+                    max = Math.max(max, j-i);
+                    while(i<=j && tempAllowedOnes<0){
+                        if(arr[i]==0){
+                            tempAllowedOnes++;
+                        }
+                        i++;
+                    }
+                }
+                j++;
+            }
+        }
+        max = Math.max(max, j - i);
+        return max;
     }
 }
---
+++ src/Leet/minimumSwapToSortArray.java
@@ -0,0 +1,11 @@
+package Leet;
+
+public class minimumSwapToSortArray {
+    public static void main(String[] args) {
+        new minimumSwapToSortArray().run();
+    }
+
+    private void run() {
+        
+    }
+}
